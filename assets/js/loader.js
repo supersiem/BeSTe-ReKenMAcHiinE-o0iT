506ee937ae43df3a44e88e7c122f8411f9f13bdf
supersiem <vankeulensiem@gmail.com>
2025-03-01 18:17:59 +0100

    24

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -5,7 +5,7 @@ let antwoord = document.getElementById("antwoord");
 let output = "";
 let nummers = ["1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "=", "0", "DEL", "/"];
 const nummbers_old = [...nummers];
-const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", ".", "(", ")", ";", ":", "?", "!", ","];
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", ".", "(", ")", ";", ":", "?", "!", ",", "24", "24", "24", "24"];
 let user_input = [];
 
 
